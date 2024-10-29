a633da9b1bdb27993c87ff02a8ef36c7ea90895f
Yana Ivanova <yanaivashka@yandex.ru>
2024-10-29 20:27:57 +0300

    added base types

--- src/shared/model/types.ts
+++ src/shared/model/types.ts
@@ -1,25 +1,23 @@
-export { Dayjs } from "dayjs";
 export interface Meeting {
   meeting_id: number;
   expert_id: number;
-  client_id: number;
+  client_id: number[];
   slot_id: number;
+  date: string;
   start_time: string;
   end_time: string;
   meeting_type: string;
   meeting_status: string;
 }
 
-export type MeetingMaterial = {
-  material_id: number;
-  meeting_id: number;
-  material_link: string;
-  description: string;
+export type BaseSlot = {
+  date: string;
+  start_time: string;
+  end_time: string;
 };
-
 export interface Slot {
-  slot_id: number;
-  user_id: number;
+  creator_id: number;
+  date: string;
   start_time: string;
   end_time: string;
   description: string;
@@ -27,16 +25,22 @@ export interface Slot {
   is_availible: boolean;
 }
 
-export interface MeetingResult {
-  result_id: number;
+export interface Material {
+  material_id: number;
   meeting_id: number;
   material_link: string;
-  description: string;
+}
+
+export interface Comment {
+  comment_id: number;
+  meeting_id: number;
+  user_id: number;
+  create_time: string;
+  comment_text: string;
 }
 
 export interface Review {
   review_id: number;
   meeting_id: number;
   raiting: number;
-  comment: string;
 }
