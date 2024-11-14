b2cabe2622c6fe81517660c79357f765f4834cba
Yana Ivanova <yanaivashka@yandex.ru>
2024-11-14 03:05:19 +0300

    fix export

--- src/shared/lib/hooks/index.ts
+++ src/shared/lib/hooks/index.ts
@@ -1,4 +1,3 @@
 export { useModal } from "./useModal";
 export { useDebounce } from "./useDebounce";
-export { useSuggestions } from "./useSuggestions";
 export { useAppDispatch, useAppSelector } from "./useRedux";
